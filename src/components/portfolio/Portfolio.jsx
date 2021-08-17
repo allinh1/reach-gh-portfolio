89aae8ed6ee868b9eaf71fb209da5a99b9f22a9b
allinh1 <dl.nguyen1322@gmail.com>
2021-08-17 11:52:31 +1000

    added portfolio page

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -3,7 +3,22 @@ import styles from "./Portfolio.module.scss";
 const Portfolio = () => {
     return (
         <div className={styles.portfolio} id="portfolio">
+            <h1>Portfolio</h1>
+            <ul>
+                <li>Featured</li>
+                <li>Client Project</li>
+                <li>Calculator</li>
+                <li>Ecommerce Shop</li>
+                <li>Books API</li>
+            </ul>
             
+            <div className={styles.container}>
+                <div className="item"></div>
+                <img src="https://via.placeholder.com/150" alt="" />
+                <h3>Project 1</h3>
+            </div>
+
+
         </div>
     )
 }
