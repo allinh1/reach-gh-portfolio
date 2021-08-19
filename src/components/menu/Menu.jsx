22a220335b5b25bab69bbbb587b26bdf44188a97
allinh1 <dl.nguyen1322@gmail.com>
2021-08-19 13:12:44 +1000

    prettier changes

--- src/components/menu/Menu.jsx
+++ src/components/menu/Menu.jsx
@@ -1,39 +1,34 @@
 // Passing props
 
-import styles from "./Menu.module.scss"
-import React from 'react'
+import styles from "./Menu.module.scss";
+import React from "react";
 
-const Menu = ({menuOpen , setMenuOpen}) => {
+const Menu = ({ menuOpen, setMenuOpen }) => {
+  const classes = menuOpen ? [styles.menu, styles.active] : [styles.menu];
 
-    const classes = menuOpen
-        ? [styles.menu, styles.active]
-        : [styles.menu]
+  const handleClick = () => setMenuOpen(false);
 
+  return (
+    <div className={classes.join(" ")}>
+      <ul>
+        <li onClick={handleClick}>
+          <a href="#intro">Top</a>
+        </li>
+        <li onClick={handleClick}>
+          <a href="#portfolio">Porfolio</a>
+        </li>
+        <li onClick={handleClick}>
+          <a href="#projects">Projects</a>
+        </li>
+        <li onClick={handleClick}>
+          <a href="#skills">Skills</a>
+        </li>
+        <li onClick={handleClick}>
+          <a href="#contact">Get In Touch</a>
+        </li>
+      </ul>
+    </div>
+  );
+};
 
-    const handleClick = ()=>setMenuOpen(false);
-
-    return (
-        <div className={classes.join(" ")}>
-            <ul>
-                <li onClick={handleClick} >
-                    <a href="#intro">Top</a>
-                </li>
-                <li onClick={handleClick}>
-                    <a href="#portfolio">Porfolio</a>
-                </li>
-                <li onClick={handleClick}>
-                    <a href="#projects">Projects</a>
-                </li>
-                <li onClick={handleClick}>
-                    <a href="#skills">Skills</a>
-                </li>
-                <li onClick={handleClick}>
-                    <a href="#contact">Get In Touch</a>
-                </li>
-            </ul>
-            
-        </div>
-    )
-}
-
-export default Menu
+export default Menu;
--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -2,107 +2,91 @@
 
 import { useState } from "react";
 import styles from "./Projects.module.scss";
-import ArrowBackIosIcon from '@material-ui/icons/ArrowBackIos';
-import ArrowForwardIosIcon from '@material-ui/icons/ArrowForwardIos';
+import ArrowBackIosIcon from "@material-ui/icons/ArrowBackIos";
+import ArrowForwardIosIcon from "@material-ui/icons/ArrowForwardIos";
 
 const Projects = () => {
-    const [currentSlide, setCurrentSlide] = useState(0);
-    const data = [
-      {
-        id: "1",
-        title: "Client Project",
-        desc:
-          "Stuff about the client project",
-        img:
-        "",
-        link:""
-      },
-      {
-        id: "2",
-        title: "Calculator",
-        desc:
-          "Things about a calculator",
-        link:
-          "https://github.com/allinh1/calculator",
-      },
-      {
-        id: "3",
-        title: "Google Books API",
-        desc:
-          "Fetching Stuff",
-          link:
-        "https://github.com/allinh1/google-books-api",
-      },
-      {
-        id: "4",
-        title: "Ecom Store",
-        desc:
-          "E-commerce project w/ react + firebase",
-        link:
-        "https://github.com/allinh1/ecom-project",
-      },
-      {
-        id: "5",
-        title: "Morse Translator",
-        desc:
-          "A morse code translator which can convert plain text into encrypted form as morse code and also it can convert morse code back into plain text",
-        link:
-        "https://github.com/allinh1/morse-translator",
-      },
-    ];
-  
-    const handleClick = (slides) => { 
-        slides === "nextSlide"
-        ? setCurrentSlide(currentSlide > 0 ? currentSlide - 1 : 4)
-        : setCurrentSlide(currentSlide < data.length - 1 ? currentSlide + 1 : 0);
-    };
+  const [currentSlide, setCurrentSlide] = useState(0);
+  const data = [
+    {
+      id: "1",
+      title: "Client Project",
+      desc: "Stuff about the client project",
+      img: "",
+      link: "",
+    },
+    {
+      id: "2",
+      title: "Calculator",
+      desc: "Things about a calculator",
+      link: "https://github.com/allinh1/calculator",
+    },
+    {
+      id: "3",
+      title: "Google Books API",
+      desc: "Fetching Stuff",
+      link: "https://github.com/allinh1/google-books-api",
+    },
+    {
+      id: "4",
+      title: "Ecom Store",
+      desc: "E-commerce project w/ react + firebase",
+      link: "https://github.com/allinh1/ecom-project",
+    },
+    {
+      id: "5",
+      title: "Morse Translator",
+      desc: "A morse code translator which can convert plain text into encrypted form as morse code and also it can convert morse code back into plain text",
+      link: "https://github.com/allinh1/morse-translator",
+    },
+  ];
 
-    // if(!Array.isArray(data) || data.length <= 0){
-    //     return null;
-    // }
+  const handleClick = (slides) => {
+    slides === "nextSlide"
+      ? setCurrentSlide(currentSlide > 0 ? currentSlide - 1 : 4)
+      : setCurrentSlide(currentSlide < data.length - 1 ? currentSlide + 1 : 0);
+  };
 
-    return (
+  // if(!Array.isArray(data) || data.length <= 0){
+  //     return null;
+  // }
 
-
-        <div className={styles.projects} id="projects">
-        <div
-          className={styles.slider}
-          style={{ transform: `translateX(${-currentSlide * 100}vw)` }}
-        >
-          {data.map((data) => (
-            <div className={styles.container}>
-              <div className={styles.item}>
-                <div className={styles.left}>
-                  <div className={styles.leftContainer}>
-                    {/* <div className={styles.imgContainer}>
+  return (
+    <div className={styles.projects} id="projects">
+      <div
+        className={styles.slider}
+        style={{ transform: `translateX(${-currentSlide * 100}vw)` }}
+      >
+        {data.map((data) => (
+          <div className={styles.container}>
+            <div className={styles.item}>
+              <div className={styles.left}>
+                <div className={styles.leftContainer}>
+                  {/* <div className={styles.imgContainer}>
                     </div> */}
-                    <h2>{data.title}</h2>
-                    <p>{data.desc}</p>
-                    <span>{data.link}</span>
-                  </div>
-                </div>
-                <div className={styles.right}>
-                  <img
-                    src=""
-                    alt=""
-                  /> 
+                  <h2>{data.title}</h2>
+                  <p>{data.desc}</p>
+                  <span>{data.link}</span>
                 </div>
               </div>
+              <div className={styles.right}>
+                <img src="" alt="" />
+              </div>
             </div>
-          ))}
-          
-        </div>
-        <ArrowBackIosIcon
-            className={`${styles.left} ${styles.arrow}`}
-            onClick={() => handleClick("nextSlide")}/>
-
-        <ArrowForwardIosIcon
-            className={`${styles.right} ${styles.arrow}`}
-            onClick={() => handleClick()}/>
+          </div>
+        ))}
       </div>
-        
-    )
-}
+      <ArrowBackIosIcon
+        className={`${styles.left} ${styles.arrow}`}
+        onClick={() => handleClick("nextSlide")}
+      />
 
-export default Projects
+      <ArrowForwardIosIcon
+        className={`${styles.right} ${styles.arrow}`}
+        onClick={() => handleClick()}
+      />
+    </div>
+  );
+};
 
+export default Projects;
